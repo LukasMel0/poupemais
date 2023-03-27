8aeca44fd5c1ecc7403c85d24f4bc1628ac0995a
Lucas Melo <lucasmdsmart@gmail.com>
2023-03-27 16:07:22 -0300

    testes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+      <TouchableOpacity 
+      
+      style={styles.bnt}>
+        <Text style={styles.text}>Click em mim</Text>
+      </TouchableOpacity>
+      <Text>{}</Text>
+      </View>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -18,4 +29,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bnt:{
+    borderWidth: 1,
+    backgroundColor: '#2e0331',
+    padding: 15,
+    borderRadius: 10,
+    shadowOpacity: 1
+  },
+
+  text:{
+    fontSize: 25,
+    fontWeight: 'bold'
+  }
 });
